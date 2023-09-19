7dc42dfa04728c2247b2de20655f236d07ed7db3
strudent <student@gnu.ac.in>
2023-09-19 22:13:19 +0530

    xhbgaugfri

--- app/src/main/java/com/example/mad_practical_6_21012021051/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_6_21012021051/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.example.mad_practical_6_21012021051
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var button : Button = findViewById(R.id.button)
-        var button1:Button = findViewById(R.id.button2)
+        var button : Button = findViewById(R.id.button2)
+        var button1:Button = findViewById(R.id.button3)
         button.setOnClickListener {
             playpause()
         }
